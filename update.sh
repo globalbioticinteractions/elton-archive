c40b755e9994415ae5bf9f128876a24649e0bd74
jhpoelen <jhpoelen+git@gmail.com>
2017-09-26 06:23:07 +0200

    git-annex automatic sync

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# usage: ./update.sh [src] [target git annex repo]
+
+usage() {
+  echo >&2 "$@"
+  echo "usage: ./update.sh [elton dataset dir] [target git annex repo dir]"
+  exit 1
+}
+
+[ "$#" -eq 2 ] || usage "needs two parameters" 
+
+[ -d $1 ] || usage "dataset dir [$1] does not exist"
+[ -d $2 ] || usage "git annex repo dir [$2] does not exist"
+
+cp -R $1 $2
+cd $2
+git annex add --exclude=*/access.tsv
+
+source $2/.archive.env
+git annex copy --to=internet-archive
+echo "git annex sync"
